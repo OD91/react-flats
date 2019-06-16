3f611a930dbad2f5a0d1f181d6b8a326ef32b5aa
ole dening <ole.dening@gmail.com>
2019-06-16 20:14:50 +0200

    edited

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -35,11 +35,7 @@ class App extends Component {
         />
         <div className="map-container">
           <GoogleMapReact
-<<<<<<< HEAD
-            bootstrapURLKeys={{key:'must be replaced'}}
-=======
             bootstrapURLKeys={{key:'put your api key here'}}
->>>>>>> 9f7dbf8746089e061b193cdf5cc71838e3c11f18
             defaultCenter={this.center()}
             defaultZoom={12}>
             <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
